902a59c043b41a915a77856f9c7b5beb91c303f3
sFiFty <alexandr.rudin.my@gmail.com>
2024-02-29 13:01:01 +0100

    update rich text to insert images

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useMemo, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
@@ -7,16 +7,46 @@ interface Props {
   value?: string;
 }
 
-const modules = {
-  toolbar: [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [{ align: ["right", "center", "justify"] }],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["link", "image", "code-block"],
-  ],
-};
-
 export const RichTextEditor: FC<Props> = ({ onChange, value }) => {
-  return <ReactQuill value={value} onChange={onChange} modules={modules} />;
+  const quillRef = useRef<ReactQuill>(null);
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ align: ["right", "center", "justify"] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["link", "image", "code-block"],
+          ["image"],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    }),
+    []
+  );
+
+  function imageHandler() {
+    if (!quillRef.current) return;
+
+    const editor = quillRef.current.getEditor();
+    const range = editor.getSelection();
+    const value = prompt("Please enter the image URL");
+
+    if (value && range) {
+      editor.insertEmbed(range.index, "image", value, "user");
+    }
+  }
+
+  return (
+    <ReactQuill
+      ref={quillRef}
+      theme="snow"
+      defaultValue={value}
+      onChange={onChange}
+      modules={modules}
+    />
+  );
 };
